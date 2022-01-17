4d6970bb7c71746ba0b61122c6c3710f622c3020
VinceBaylon <vincentbaylon@gmail.com>
2022-01-17 10:41:10 -0800

    Working on checking words

--- src/App.js
+++ src/App.js
@@ -1,25 +1,278 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import words from './Words'
+import letters from './Letters'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentWord, setCurrentWord] = useState('')
+	const [currentGuess, setCurrentGuess] = useState('')
+	const [guesses, setGuesses] = useState([currentGuess])
+	const [gameStatus, setGameStatus] = useState('')
+
+	const firstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+	const secondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+	const thirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+	useEffect(() => {
+		let randomWord = words[Math.floor(Math.random() * words.length)]
+		setCurrentWord(randomWord)
+		console.log(randomWord)
+	}, [])
+
+	const handleDelete = () => {
+		if (gameStatus === '') {
+			setCurrentGuess(currentGuess.slice(0, -1))
+		} else {
+		}
+	}
+
+	const handleEnter = () => {
+		if (currentGuess.length === 5) {
+			if (gameStatus === '') {
+				if (currentGuess === currentWord) {
+					setGameStatus('WIN')
+				} else {
+					for (let i = 0; i < currentGuess.length; i++) {
+						if (currentGuess[i] === currentWord[i]) {
+							letters[currentGuess[i]].correct = true
+						} else if (currentWord.indexOf(currentGuess[i]) !== -1) {
+							letters[currentGuess[i]].present = true
+						} else {
+							letters[currentGuess[i]].absent = true
+						}
+					}
+
+					console.log(letters)
+					setGuesses([...guesses, currentGuess])
+					setCurrentGuess('')
+				}
+			} else {
+			}
+		}
+	}
+
+	const handleChar = (c) => {
+		if (currentGuess.length < 5 && guesses.length < 7) {
+			let current = currentGuess + c
+			setCurrentGuess(current)
+		}
+	}
+
+	useEffect(() => {}, [gameStatus])
+
+	useEffect(() => {
+		const listener = (e) => {
+			if (e.code === 'Enter') {
+				handleEnter()
+			} else if (e.code === 'Backspace') {
+				handleDelete()
+			} else {
+				const key = e.key.toUpperCase()
+				if (key.length === 1 && key >= 'A' && key <= 'Z') {
+					handleChar(key)
+				}
+			}
+		}
+		window.addEventListener('keyup', listener)
+		return () => {
+			window.removeEventListener('keyup', listener)
+		}
+	}, [])
+
+	const handleClick = (e) => {
+		if (e.target.value === 'ENTER') {
+			handleEnter()
+		} else if (e.target.value === 'DELETE') {
+			handleDelete()
+		} else {
+			handleChar(e.target.value)
+		}
+	}
+
+	const splitGuess = currentGuess.split('')
+	const emptyTiles = Array.from(Array(5 - splitGuess.length))
+	const onlyEmptyTiles = Array.from(Array(5))
+
+	const displayTiles = splitGuess.map((g) => {
+		if (gameStatus === 'WIN') {
+			return (
+				<div
+					key={g}
+					className='flex justify-center items-center font-bold text-3xl md:text-4xl h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700 bg-green-500'
+				>
+					<h1 className='text-white'>{g}</h1>
+				</div>
+			)
+		} else {
+			return (
+				<div
+					key={g}
+					className='flex justify-center items-center font-bold text-3xl md:text-4xl h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700'
+				>
+					<h1 className='text-white'>{g}</h1>
+				</div>
+			)
+		}
+	})
+
+	const displayEmptyTiles = emptyTiles.map((_, i) => {
+		return (
+			<div
+				key={i}
+				className='h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700'
+			></div>
+		)
+	})
+
+	const displayOnlyEmptyTiles = onlyEmptyTiles.map((_, i) => {
+		return (
+			<div
+				key={i}
+				className='h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700'
+			></div>
+		)
+	})
+
+	const displayCurrentRow = [currentGuess].map((t) => {
+		return (
+			<div key={t} className='flex flex-row gap-1'>
+				{displayTiles}
+				{displayEmptyTiles}
+			</div>
+		)
+	})
+
+	const displayRows = guesses.map((g, i) => {
+		return (
+			<div key={i} className='flex flex-row gap-1'>
+				{g.split('').map((e) => {
+					if (letters[e].correct === true) {
+						return (
+							<div
+								key={e}
+								className='flex justify-center items-center font-bold text-3xl md:text-4xl h-12 w-12 md:h-20 md:w-20 border-2 border-green-700 bg-green-500'
+							>
+								<h1 className='text-white'>{e}</h1>
+							</div>
+						)
+					} else if (letters[e].present === true) {
+						return (
+							<div
+								key={e}
+								className='flex justify-center items-center font-bold text-3xl md:text-4xl h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700 bg-yellow-500'
+							>
+								<h1 className='text-white'>{e}</h1>
+							</div>
+						)
+					} else if (letters[e].absent === true) {
+						return (
+							<div
+								key={e}
+								className='flex justify-center items-center font-bold text-3xl md:text-4xl h-12 w-12 md:h-20 md:w-20 border-2 border-slate-700 bg-slate-900'
+							>
+								<h1 className='text-white'>{e}</h1>
+							</div>
+						)
+					}
+				})}
+			</div>
+		)
+	})
+
+	const emptyRows = Array.from(Array(6 - guesses.length))
+
+	const displayEmptyRows = emptyRows.map((_, i) => {
+		return (
+			<div key={i} className='flex flex-row gap-1'>
+				{displayOnlyEmptyTiles}
+			</div>
+		)
+	})
+
+	const displayRow = (row) =>
+		row.map((e) => {
+			if (letters[e].correct === true) {
+				return (
+					<button
+						key={e}
+						value={e}
+						className='h-8 w-6 md:h-10 md:w-8 lg:h-12 lg:w-10 rounded-sm text-white bg-green-500 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						{e}
+					</button>
+				)
+			} else if (letters[e].present === true) {
+				return (
+					<button
+						key={e}
+						value={e}
+						className='h-8 w-6 md:h-10 md:w-8 lg:h-12 lg:w-10 rounded-sm text-white bg-yellow-500 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						{e}
+					</button>
+				)
+			} else if (letters[e].absent === true) {
+				return (
+					<button
+						key={e}
+						value={e}
+						className='h-8 w-6 md:h-10 md:w-8 lg:h-12 lg:w-10 rounded-sm text-white bg-slate-900 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						{e}
+					</button>
+				)
+			} else {
+				return (
+					<button
+						key={e}
+						value={e}
+						className='h-8 w-6 md:h-10 md:w-8 lg:h-12 lg:w-10 rounded-sm text-white bg-slate-500 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						{e}
+					</button>
+				)
+			}
+		})
+
+	return (
+		<div className='flex flex-col items-center justify-center h-full min-h-screen star-background relative'>
+			<h1 className='p-5 text-xl md:text-4xl font-bold text-slate-300 absolute top-0'>
+				STAR WARS WORDLE
+			</h1>
+			<div className='flex items-center h-full absolute'>
+				<div className='flex flex-col gap-1'>
+					{displayRows}
+					{displayCurrentRow}
+					{displayEmptyRows}
+				</div>
+			</div>
+			<div className='p-5 absolute bottom-0 items-center flex flex-col gap-1'>
+				<div className='flex flex-row gap-1'>{displayRow(firstRow)}</div>
+				<div className='flex flex-row gap-1'>{displayRow(secondRow)}</div>
+				<div className='flex flex-row gap-1'>
+					<button
+						value='ENTER'
+						className='h-8 w-14 md:h-10 md:w-14 lg:h-12 lg:w-16 rounded-sm text-white bg-slate-500 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						ENTER
+					</button>
+					{displayRow(thirdRow)}
+					<button
+						value='DELETE'
+						className='h-8 w-12 md:h-10 md:w-14 lg:h-12 lg:w-16 rounded-sm text-white bg-slate-500 font-semibold drop-shadow-lg'
+						onClick={handleClick}
+					>
+						DEL
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
